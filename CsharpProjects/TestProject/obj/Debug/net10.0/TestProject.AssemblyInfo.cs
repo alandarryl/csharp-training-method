eaf4b03f103c8290024dc6b2605da56a1d4e2970
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-02-22 10:44:46 +0100

    add a time managing function

--- CsharpProjects/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38453ef8fc8f274ebdea3fc5f1d2ceb867d8c3f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6cbb59dc0f6f3127f8dcee22c2e0a11477e2db4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
