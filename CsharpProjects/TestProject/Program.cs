38453ef8fc8f274ebdea3fc5f1d2ceb867d8c3f6
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-02-22 10:08:34 +0100

    add a function display random numbers

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,3 +1,17 @@
 ﻿
-Console.WriteLine("hellow world");
+// Console.WriteLine("hellow world");
 
+void DisplayRandomNumbers()
+{
+    Random random = new Random();
+
+    for(int i = 0; i < 5; i++)
+    {
+        Console.Write($" {random.Next(1, 100 )} ");
+    }
+    Console.WriteLine();
+
+}
+
+Console.WriteLine("Generating random numbers");
+DisplayRandomNumbers();
